f361b58c0c0c53158626f657be9eb1f8c11b9847
Serhii Voitenko <serhii.voitenko98@gmail.com>
2021-09-19 18:26:55 +0300

    update app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { AccordionModule } from './components/accordion/accordion.module';
+import { CardModule } from './components/card/card.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,16 +8,24 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { ToolbarModule } from './components/toolbar/toolbar.module';
 import { AppRoutingModule } from './app-routing.module';
+import { ProgressBarModule } from './components/progress-bar/progress-bar.module';
+import { StarRatingsModule } from './components/star-ratings/star-ratings.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CardModule,
+    FormsModule,
     BrowserModule,
+    ToolbarModule,
+    AccordionModule,
     AppRoutingModule,
     FontAwesomeModule,
-    ToolbarModule,
+    ProgressBarModule,
+    StarRatingsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
