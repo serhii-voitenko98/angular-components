04ee53531ecf7a556736789e3a08b3a28de1b1f6
Serhii Voitenko <s.voitenko@spd-ukraine.com>
2021-09-19 15:59:51 +0300

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { AppComponent } from './app.component';
+import { ToolbarModule } from './components/toolbar/toolbar.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    ToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
